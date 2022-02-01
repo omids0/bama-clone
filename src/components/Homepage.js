fbff3b86b29cf200699f876e56c93acc5bab8393
omids0 <soheilniao@gmail.com>
2022-02-02 03:15:33 +0330

    basic-homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
 function Homepage() {
-  return <div className="homepage">
-      homepage
-  </div>;
+      return <div className="homepage">
+            <div className="homepage-tabs">
+                  <div className='homepage-tab-section'>
+                        <a href="#tab1" className='tab'>میانبر</a>
+                  </div>
+                  <div className='homepage-tab-section'>
+                        <a href="#tab2" className='tab'>برند</a>
+                  </div>
+                  <div className='homepage-tab-section'>
+                        <a href="#tab3" className='tab'>شاسی</a>
+                  </div>
+                  <div>
+                        <a href="#tab4" className='tab'>قیمت</a>
+                  </div>
+            </div>
+            <div className="homepage-details">
+            </div>
+      </div>;
 }
 
 export default Homepage;
